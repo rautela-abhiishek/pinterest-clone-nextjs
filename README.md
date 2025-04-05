f1ffddf14dc4c0f9398cf4abbf711a369c10dd09
rautela-abhiishek <abhisheksinghratela72@gmail.com>
2025-04-05 11:20:09 +0530

    imported project structure and clear all the non usable folders and codes

--- README.md
+++
@@ -1,2 +0,0 @@
-# pinterest-clone-nextjs
-Build a full stack Pinterest clone using Next.js 13, React.js, Next Auth, Tailwind CSS, and Firebase.
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    
+    <h2>Welcome to My Pintrest Clone</h2>
+    </>
+
+  )
+}
